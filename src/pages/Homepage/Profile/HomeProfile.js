0525e7e6751dd55dddecfa4bc2c1e17c605ca31c
odusdegreat <tobiodubote@gmail.com>
2025-07-13 19:12:15 +0100

    fix: removed some images

--- src/pages/Homepage/Profile/HomeProfile.js
+++ src/pages/Homepage/Profile/HomeProfile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../../../assets/images/kenny.JPEG";
+import profile from "../../../assets/images/oduspic.jpg";
 
 const HomeProfile = () => {
   return (
